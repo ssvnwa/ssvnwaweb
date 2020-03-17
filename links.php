eae792ed6d849420a456c1d22a114cf87cee4ad4
Otis Michaud <omichaud@gmail.com>
2020-03-17 14:44:44 -0500

    Adding link to scaleschords
    adding link to scales-chords.com

--- links.php
+++ links.php
@@ -25,17 +25,22 @@ echo "       </div>\n";
 echo "    </div>\n";
 echo "    <div class='row'  align='center'>\n";
 echo "       <div class='col'>\n";
-echo "          <p><a href='http://bvstrings.org' target='_blank'><img src='./assets/img/BVS-Basic-Wings.jpg' alt='BVStrings' style='width: 60%;'></img></p>\n";
-echo "          <p><a href='http://bvstrings.org' target='_blank'>Several of our members are also involved with BVStrings. Please visit their page and say hello!<br>They have songs and events posted on their site.</a></p>\n";
+echo "          <p><a href='https://bvstrings.org' target='_blank'><img src='./assets/img/BVS-Basic-Wings.jpg' alt='BVStrings' style='width: 60%;'></img></p>\n";
+echo "          <p><a href='https://bvstrings.org' target='_blank'>Several of our members are also involved with BVStrings. Please visit their page and say hello!<br>They have songs and events posted on their site.</a></p>\n";
 echo "       </div>\n";
 echo "    </div>\n";
 echo "    <div class='row'  align='center'>\n";
 echo "       <div class='col'>\n";
-echo "          <p><a href='http://ultimate-guitar.com' target='_blank'><img src='./assets/img/ultimateguitar.png' alt='UG' style='width: 60%;'></img></p>\n";
-echo "          <p><a href='http://ultimate-guitar.com' target='_blank'>Several of our members use Ultimate-Guitar site and GuitarPro phone application. <br>Free accounts are available in addition to paid subscription options.</a></p>\n";
+echo "          <p><a href='https://ultimate-guitar.com' target='_blank'><img src='./assets/img/ultimateguitar.png' alt='UG' style='width: 60%;'></img></p>\n";
+echo "          <p><a href='https://ultimate-guitar.com' target='_blank'>Several of our members use Ultimate-Guitar site and GuitarPro phone application. <br>Free accounts are available in addition to paid subscription options.</a></p>\n";
 echo "       </div>\n";
 echo "    </div>\n";
-
+echo "    <div class='row'  align='center'>\n";
+echo "       <div class='col'>\n";
+echo "          <p><a href='https://scales-chords.com' target='_blank'><img src='./assets/img/scaleschords.png' alt='Scales-Chords' style='width: 60%;'></img></p>\n";
+echo "          <p><a href='https://scales-chords.com' target='_blank'>Scales-Chords.com is a valuable free resource for any musician hoping to learn to play any instrument.</a></p>\n";
+echo "       </div>\n";
+echo "    </div>\n";
 echo " </div>\n";
 echo "<!-- End: Links -->\n"; 
 // footer HTML and JavaScript codes
