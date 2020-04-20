4075342800abf9014fb46d18adbd4704125f0029
Otis Michaud <omichaud@gmail.com>
2020-04-20 13:26:39 -0500

    Updating welcome page with covid advice
    Updating welcome page with covid advice

--- index.php
+++ index.php
@@ -12,12 +12,14 @@ $require_login=false;
 include_once 'layout_head.php';
 // content 
     echo "<header class='masthead' style='background-image: url(&quot;assets/img/SSVNWALogo.png&quot;);background-size: auto;'>";
-    echo "<div class='container'>";
+	echo "<div class='container'>";
     echo "  <div class='intro-text'>";
     echo"      <div class='intro-lead-in'><span>Welcome to the Solder Songs and Voices of Northwest Arkansas Website!</span></div>";
-    echo "        <div class='intro-heading text-uppercase'><span style='font-size: 30px;'>Thanks for stopping in!</span></div>";
-    echo " 		  <div class='intro-lead-in'><span style='color: #eeebeb;filter: brightness(148%);background-color: #0a0a0a;'>The mission of Soldier Songs &amp; Voices NWA is to foster a creative and supportive community of veterans through music instruction, performance, and outreach.</span></div>";
-    echo "    </div>";
+    echo "     <div class='intro-heading text-uppercase'><span style='font-size: 25px;'>Thanks for stopping in!</span></div>";
+    echo " 	   <div class='intro-lead-in'><span style='font-size: 20px;color: #eeebeb;filter: brightness(148%);background-color: #0a0a0a;'>The mission of Soldier Songs &amp; Voices NWA is to foster a creative and supportive community of veterans through music instruction, performance, and outreach.</span></div>";	
+    echo "     <div class='intro-heading text-uppercase'><span style='font-size: 25px;'><button type='button' style='background-color: Orange;'>COVID 19 UPDATE</button></span></div>";
+    echo " 	   <div class='intro-lead-in'><span style='font-size: 20px;color: #eeebeb;filter: brightness(148%);background-color: #0a0a0a;'>SSVNWA has currently suspended all live practice and performance events supporting efforts to contain the COVID-19 pandemic.We held our monthly board meeting via hangouts and are exploring options to resume lessons and performances remotely.</span></div>";
+    echo "  </div>";
     echo "</header>";
 
 // footer HTML and JavaScript codes
